f668d66951a93020b078804e49d61bf38e2cf37e
jsh1588 <jsh5679@naver.com>
2018-11-30 21:53:11 +0900

    2array store

--- simplehtmldom_1_5/web_crawling.php
+++ simplehtmldom_1_5/web_crawling.php
@@ -6,6 +6,7 @@
         
     $html = file_get_html("https://datalab.naver.com/");
 	$i = 0;
+	$o = -1;
     if($html)
     {
         
@@ -16,15 +17,32 @@
     <rss version="2.0">
         <channel>
 <?
-        foreach($html->find('li[class=list]') as $article) {
-            $name    = $article->find('span.title', 0)->plaintext;
-			if($name != null){
-				$name1[$i] = $name;
-				echo $name1[$i];
-				echo "\n";
+        foreach($html->find('div[class=rank_inner v2]') as $article) {
+			$date    = $article->find('span.title_cell', 0)->plaintext;
+			if($date != null && $o <=10){
+				$o = $o+1;
+				$i = 0;
+				echo $o;
+				echo " ";
 				echo $i;
+				echo " ";
+				$list[$o][$i] = $date;
+				echo $list[$o][$i];
+				echo "\n";
+			}
+			$i = 1;
+			foreach($article->find('li[class=list]') as $article1) {
+			$name    = $article1->find('span.title', 0)->plaintext;
+			if($name != null){
+				echo $o;
+				echo " ";
+				echo $i;
+				echo " ";
+				$list[$o][$i] = $name;
+				echo $list[$o][$i];
 				echo "\n";
 				$i = $i+1;
+			}
 			}
         }
 ?>
