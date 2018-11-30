200c8fad962e6f470b1d1559ea4680217cf91476
jsh1588 <jsh5679@naver.com>
2018-11-30 18:35:35 +0900

    web page down

--- web_crawling.php
+++ web_crawling.php
@@ -1,7 +1,6 @@
 #!/usr/bin/php
 <?
-include_once('simplehtmldom_1_5/simple_html_dom.php');
 
-$html = file_get_html('http://www.naver.com/');
-#$html=str_get_html('http://www.naver.com/');
+$html = file_get_contents('http://www.naver.com/');
+echo $html;
 ?>
