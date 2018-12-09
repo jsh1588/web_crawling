f1eedea56e4d473835bf04278e50e36435454794
jsh1588 <jsh5679@naver.com>
2018-12-09 15:09:48 +0900

    db login, update

---
+++ web_crawling_confirm.php
@@ -0,0 +1,79 @@
+#!/usr/bin/php
+
+<?
+    include "/home/jsh1588/simplehtmldom_1_5/simple_html_dom.php";    
+ 
+    /*if(!$_GET[page]) $page = 1;
+    else $page = $_GET[page];*/
+        
+    $html = file_get_html("https://datalab.naver.com/");
+	$i = 0;
+	$o = -1;
+    if($html)
+    {
+        
+        header('Content-Type: text/xml; charset=utf-8');
+	$servername = "localhost";
+	$username = "root";
+	$password = "Jsh1588!";
+	$dbname = "web_crawling";
+
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	mysqli_query($conn, "set names utf8");
+
+	if($conn -> connect_error){
+		die("connection failed: ".$conn->connect_error);
+	}
+
+        foreach($html->find('div[class=rank_inner v2]') as $article) {
+			$date    = $article->find('span.title_cell', 0)->plaintext;
+			if($date != null && $o <=10){
+				$o = $o+1;
+				$i = 0;
+				echo $o;
+				echo " ";
+				echo $i;
+				echo " ";
+				$date_remove = substr($date, 0, 10);
+				$list[$o][$i] = $date_remove;
+				echo $list[$o][$i];
+				echo "\n";
+			}
+			$i = 1;
+			foreach($article->find('li[class=list]') as $article1) {
+			$name    = $article1->find('span.title', 0)->plaintext;
+			if($name != null){
+				echo $o;
+				echo " ";
+				echo $i;
+				echo " ";
+				$list[$o][$i] = $name;
+				echo $list[$o][$i];
+				echo "\n";
+				$i = $i+1;
+				}
+			}
+        }
+	for($o=0; $o<12; $o++)
+	{
+		for($i=1; $i<11; $i++)
+		{
+			$sql = "INSERT INTO crawling(date, rank, name)
+				VALUES('{$list[$o][0]}', '{$i}', '{$list[$o][$i]}')";
+			if($conn->query($sql) == TRUE)
+			{
+				//echo "success!!";
+			}
+			else
+			{
+				echo "error: ".$sql."<br>".$conn->error;
+			}
+		}
+	}
+	$conn->close();
+
+    }
+    else
+        echo "error document";
+?>
+
