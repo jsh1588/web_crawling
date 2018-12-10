8105fe48d32e01b2feb20f2255416826d25787fb
jsh1588 <45190578+jsh1588@users.noreply.github.com>
2018-12-10 20:49:42 +0900

    date select->output

--- project/R.php
+++ project/R.php
@@ -44,11 +44,11 @@ $s='
 
 	date_vec = c()
 
-	for(j in seq(1:nrow(date_uni))){
+	for(j in seq(1:12)){
 		date_vec = c(date_vec, n[j,])
 	}
 
-	name_uni <- result %>% select(name)
+	name_uni <- result %>% filter(date %in% date_vec) %>% select(name)
 
 	name_uni <- unique(name_uni)
 
@@ -71,24 +71,36 @@ $s='
 					label = strong("choose item"),
 					choices = unique(result$name),
 					selected = name_vec[1]
+				),
+				selectInput(inputId="s_date",
+					label = strong("choose date"),
+					choices = date_vec,
+					selected = date_vec[1]
 				)
 			),
 			mainPanel(
 				plotOutput(outputId = "graph"),
 				textOutput(outputId = "text"),
 				h2("12 days from yesterday DATA"),
-				h2("Date : ", date_vec[12], " ~ ", date_vec[1])
+				h2("Date : ", date_vec[12], " ~ ", date_vec[1]),
+				textOutput(outputId = "text2")
 			)
 		)
 	)
 
 	server <- function(input, output){
 		output$graph <- renderPlot({
-			data_graph <- result %>% filter(name == input$type)
-                        ggplot(data_graph, aes(x=date, y=rank, group=1)) + geom_line(color="red", size=2)
+			data_graph <- result %>% filter(date %in% date_vec) %>% filter(name == input$type)
+                        ggplot(data_graph, aes(x=date, y=rank, group=1)) + geom_line(color="black", size=2) + geom_point(size=3)
 		})
 		output$text <- renderText({
-			paste("https://search.shopping.naver.com/search/all.nhn?where=all&frm=NVSCTAB&query=", input$type)
+			paste("sold: https://search.shopping.naver.com/search/all.nhn?where=all&frm=NVSCTAB&query=", input$type)
+		})
+		output$text2 <- renderText({
+			t <- result %>% filter(date %in% date_vec)
+			t <- unique(t)
+			t <- t %>% filter(input$s_date == date)
+			paste(t[, 1])
 		})
 	}
 
